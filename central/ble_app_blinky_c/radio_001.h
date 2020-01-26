89838daac6dd4d61ab4e5129404a7749090129c3
martinaalien <martin.aalien@outlook.com>
2020-01-26 16:30:29 +0100

    Initial Commit

---
+++ central/ble_app_blinky_c/radio_001.h
@@ -0,0 +1,6 @@
+#ifndef RADIO_001_H
+#define RADIO_001_H
+
+void do_rtt_measurement(void);
+
+#endif // RADIO_001_H
---
+++ peripheral/ble_app_blinky/radio_002.h
@@ -0,0 +1,6 @@
+#ifndef RADIO_002_H
+#define RADIO_002_H
+
+void do_rtt_measurement(void);
+
+#endif // RADIO_002_H
