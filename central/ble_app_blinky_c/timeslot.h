29d4762e0f835d8618727fbe6ed904fb61eccd9d
martinaalien <martin.aalien@outlook.com>
2020-03-29 15:04:48 +0200

    Central->Timeslot: Adding interrupt defines

--- central/ble_app_blinky_c/timeslot.h
+++ central/ble_app_blinky_c/timeslot.h
@@ -10,19 +10,19 @@
 #include "boards.h"
 
 #define TS_TOT_EXT_LENGTH_US    (1000000UL) /* Desired total timeslot length */
-#define TS_LEN_US               (20000UL)   /* Initial timeslot length */
-#define TS_LEN_EXTENSION_US     (20000UL)   /* Extension timeslot length */
+#define TS_LEN_US               (40000UL)   /* Initial timeslot length */
+#define TS_LEN_EXTENSION_US     (40000UL)   /* Extension timeslot length */
 #define TS_SAFETY_MARGIN_US     (250UL)     /* The timeslot activity should be finished with this much to spare. */
 #define TS_EXTEND_MARGIN_US     (500UL)     /* The timeslot activity should request an extension this long before end of timeslot. */
 
-#define TS_CALLBACK_EGU             NRF_EGU3
-#define TS_CALLBACK_EGU_IRQn        SWI3_EGU3_IRQn
-#define TS_CALLBACK_EGU_IRQHandler  SWI3_EGU3_IRQHandler
-#define TS_EGU_CALLBACK_START_INDEX 0
-#define TS_EGU_CALLBACK_END_INDEX   1
-
-typedef void (*ts_started_callback)(void);
-typedef void (*ts_stopped_callback)(void);
+#define TIMESLOT_BEGIN_EGU         NRF_EGU3
+#define TIMESLOT_BEGIN_IRQn        SWI3_EGU3_IRQn
+#define TIMESLOT_BEGIN_IRQHandler  SWI3_EGU3_IRQHandler
+#define TIMESLOT_BEGIN_IRQPriority 7
+#define TIMESLOT_END_EGU           NRF_EGU4
+#define TIMESLOT_END_IRQn          SWI4_EGU4_IRQn
+#define TIMESLOT_END_IRQHandler    SWI4_EGU4_IRQHandler
+#define TIMESLOT_END_IRQPriority   7
 
 /**@brief Radio event handler
 */
@@ -56,7 +56,7 @@ nrf_radio_signal_callback_return_param_t * radio_callback(uint8_t signal_type);
 
 /**@brief Function for initializing the timeslot API.
  */
-uint32_t timeslot_sd_init(ts_started_callback started_cb, ts_stopped_callback stopped_cb);
+uint32_t timeslot_sd_init();
 
 
 #endif
