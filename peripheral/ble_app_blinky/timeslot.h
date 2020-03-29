a44d8df1c6ac381f963de65b99ad228bc7d7dd84
martinaalien <martin.aalien@outlook.com>
2020-03-29 15:06:33 +0200

    Peripheral->timeslot: Adding interrupt defines

--- peripheral/ble_app_blinky/timeslot.h
+++ peripheral/ble_app_blinky/timeslot.h
@@ -10,10 +10,19 @@
 #include "boards.h"
 
 #define TS_TOT_EXT_LENGTH_US    (1000000UL) /* Desired total timeslot length */
-#define TS_LEN_US               (20000UL)   /* Initial timeslot length */
-#define TS_LEN_EXTENSION_US     (20000UL)   /* Extension timeslot length */
+#define TS_LEN_US               (40000UL)   /* Initial timeslot length */
+#define TS_LEN_EXTENSION_US     (40000UL)   /* Extension timeslot length */
 #define TS_SAFETY_MARGIN_US     (250UL)     /* The timeslot activity should be finished with this much to spare. */
 #define TS_EXTEND_MARGIN_US     (500UL)     /* The timeslot activity should request an extension this long before end of timeslot. */
+
+#define TIMESLOT_BEGIN_EGU         NRF_EGU3
+#define TIMESLOT_BEGIN_IRQn        SWI3_EGU3_IRQn
+#define TIMESLOT_BEGIN_IRQHandler  SWI3_EGU3_IRQHandler
+#define TIMESLOT_BEGIN_IRQPriority 7
+#define TIMESLOT_END_EGU           NRF_EGU4
+#define TIMESLOT_END_IRQn          SWI4_EGU4_IRQn
+#define TIMESLOT_END_IRQHandler    SWI4_EGU4_IRQHandler
+#define TIMESLOT_END_IRQPriority   7
 
 /**@brief Radio event handler
 */
