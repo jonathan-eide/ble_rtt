98e8521d0448002d9230e36b3e7c00398eae24a5
martinaalien <martin.aalien@outlook.com>
2020-04-18 12:42:54 +0200

    Fixes before test

--- peripheral/ble_app_blinky/rtt_parameters.h
+++ peripheral/ble_app_blinky/rtt_parameters.h
@@ -42,7 +42,7 @@
 
 /* Timeslot API defines */
 #define TS_TOT_EXT_LENGTH_US    (1000000UL) /* Desired total timeslot length */
-#define TS_LEN_US               (10000UL)   /* Initial timeslot length */
+#define TS_LEN_US               (38000UL)   /* Initial timeslot length */
 #define TS_LEN_EXTENSION_US     TS_LEN_US   /* Extension timeslot length */
 #define TS_SAFETY_MARGIN_US     (250UL)     /* The timeslot activity should be finished with this much to spare. */
 #define TS_EXTEND_MARGIN_US     (500UL)     /* The timeslot activity should request an extension this long before end of timeslot. */
